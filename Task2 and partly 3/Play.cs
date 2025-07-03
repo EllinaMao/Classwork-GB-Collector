348805e60037fcd99cc5473cdb325ef85edee318
Olga <xotaoshi@gmail.com>
2025-07-03 20:51:54 +0300

    tsdk2

---
+++ Task2 and partly 3/Play.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_and_partly_3
+{    /*Задание 2:
+Создайте класс Спектакль. Класс должен хранить такую
+информацию:
+ Название спектакля
+ Название театра
+ Жанр
+ Длительность
+ Список актёров
+Реализуйте в классе методы и свойства, необходимые для
+функционирования класса.
+Класс должен реализовывать интерфейс IDisposable. Напишите
+код для тестирования функциональности класса.
+Напишите код для вызова метода Dispose.
+    Добавьте ко второму заданию реализацию
+деструктора. Напишите код для тестирования новых
+возможностей.*/
+    internal class Play : IDisposable, IEnumerable<Play>
+    {
+        public string? Title { get; set; }
+        public string? Theater { get; set; }
+        public string? Genre { get; set; }
+        public TimeOnly? Duration { get; set; }
+        public List<string> Actors { get; set; }
+
+        private bool _disposed = false;
+
+        public Play(string title, string theater, string genre, TimeOnly duration, List<string> actors)
+        {
+            Title = title;
+            Theater = theater;
+            Genre = genre;
+            Duration = duration;
+            Actors = actors;
+        }
+
+        ~Play()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            //эээксперименты!
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                     Actors.Clear();
+                }
+                Title = null;
+                Theater = null;
+                Genre = null;
+                Duration = default;
+
+                _disposed = true;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} в {Theater}, Жанр: {Genre}, Длительность: {Duration}, Актеры: {string.Join(", ", Actors)}";
+        }
+
+        public IEnumerator<Play> GetEnumerator()
+        {
+            yield return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- Task2 and partly 3/Program.cs
+++ Task2 and partly 3/Program.cs
@@ -1,11 +1,48 @@
-﻿    namespace Task2_and_partly_3
+﻿namespace Task2_and_partly_3
 {
-    /**/
+    /*Задание 2:
+Создайте класс Спектакль. Класс должен хранить такую
+информацию:
+ Название спектакля
+ Название театра
+ Жанр
+ Длительность
+ Список актёров
+Реализуйте в классе методы и свойства, необходимые для
+функционирования класса.
+Класс должен реализовывать интерфейс IDisposable. Напишите
+код для тестирования функциональности класса.
+Напишите код для вызова метода Dispose.
+    Добавьте ко второму заданию реализацию
+деструктора. Напишите код для тестирования новых
+возможностей.*/
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Play> plays = new List<Play>();
+            plays.Add(new Play("Название1", "Театр1", "Жанр1", new TimeOnly(1, 30), new List<string> { "Актёр1", "Актёр2" }));
+            for (int i = 0; i < 10; i++)
+                using (var play = new Play("Название", "Театр", "Жанр", new TimeOnly(12, 32), new List<string> {$"Актёр {i + 1}", $"Актёр {i + 2}" }))
+                {
+                    plays.Add(play);
+                    Console.WriteLine($"{play}, {i+1}");
+                }
+            Console.ReadLine();
+            Console.Clear();   
+            try
+            {
+                foreach (var play in plays)
+                {
+                    Console.WriteLine(play);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+
+            }
         }
     }
 }
+
